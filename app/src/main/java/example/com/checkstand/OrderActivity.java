c0ce5f56619fe7d2220bf3aa3cdf608e0f0e4f4a
laizuhong <563643814@qq.com>
2015-10-10 17:04:37 +0800

    2015/10/10

--- app/src/main/java/example/com/checkstand/OrderActivity.java
+++ app/src/main/java/example/com/checkstand/OrderActivity.java
@@ -2,8 +2,10 @@ package example.com.checkstand;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import example.com.checkstand.bean.GoodOrder;
 import example.com.checkstand.bean.OrderDetail;
 
 /**
@@ -12,6 +14,7 @@ import example.com.checkstand.bean.OrderDetail;
 public class OrderActivity extends BaseActivity{
 
     List<OrderDetail> orderDetails;
+    List<GoodOrder> goodOrders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,10 @@ public class OrderActivity extends BaseActivity{
     }
 
     private void initDate(){
-
-
+        orderDetails = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            //orderDetails.add(new GoodOrder(""))
+        }
         //orderDetails.add(new OrderDetail(0,20151010,120,120,2010-10-10,"laizuhong",123456,"待处理","送货","发货"));
     }
 }
